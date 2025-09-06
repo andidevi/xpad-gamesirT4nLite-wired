7b9a2fa9947fae2a92661c00a6a239ad642b1af4
andidevi <35863827+andidevi@users.noreply.github.com>
2025-09-06 12:05:16 +0200

    Update xpad.c to support GameSir T4 Nova Lite wired

--- xpad.c
+++ xpad.c
@@ -436,7 +436,8 @@ static const struct xpad_device {
 	{ 0x3767, 0x0101, "Fanatec Speedster 3 Forceshock Wheel", 0, XTYPE_XBOX },
 	{ 0x413d, 0x2104, "Black Shark Green Ghost Gamepad", 0, XTYPE_XBOX360 },
 	{ 0xffff, 0xffff, "Chinese-made Xbox Controller", 0, XTYPE_XBOX },
-	{ 0x0000, 0x0000, "Generic X-Box pad", 0, XTYPE_UNKNOWN }
+	{ 0x0000, 0x0000, "Generic X-Box pad", 0, XTYPE_UNKNOWN },
+	{ 0x3537, 0x1041, "GameSir T4 Nova Lite wired", 0, XTYPE_XBOX360 },
 };
 
 /* buttons shared with xbox and xbox360 */
